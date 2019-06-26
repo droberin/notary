c0692e5437b8d2e7e1e04c372e1dd282a1d9285d
Roberto Salgado <robertosla@ext.inditex.com>
2019-06-26 18:39:53 +0200

    Add Notifications Manager and Telegram backend
    Too much for a single commit, I know... :)

--- notarisation.py
+++ notarisation.py
@@ -1,10 +1,15 @@
 import logging
-from flask import Flask
+from flask import Flask, request
 from os import environ
-from os.path import isfile
+from os.path import isfile, join, curdir
 from notary import Notary
+from notary.notifier.notifier import NotificationManager
+from yaml import safe_load
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
+notification_manager = NotificationManager()
 
 
 @app.route('/notary/<token>/<encryption_key>')
@@ -15,6 +20,9 @@ def decrypt_data(token, encryption_key):
     try:
         cryptographer = Notary(encryption_key)
     except ValueError:
+        notification_manager.backend_module.notify(
+            message=f'Valid token requested but malformed decryption key from {request.remote_addr}'
+        )
         return 'That key is fucked up'
     if getattr(app, '_encrypted_from_env'):
         data = environ.get('encrypted_data')
@@ -24,8 +32,17 @@ def decrypt_data(token, encryption_key):
             data_file_handler.close()
     decrypted_data = cryptographer.decrypt(data)
     if not decrypt_data:
-        logging.debug('Valid token but possible incorrect decryption key was provided... ')
-    return decrypted_data
+        message = f'Valid token requested but incorrect decryption key from {request.remote_addr}'
+        response = notification_manager.backend_module.notify(message=message)
+        logging.warning(message)
+        return message
+    message = f':WARNING: Your key has been obtained from {request.remote_addr}'
+    response = notification_manager.backend_module.notify(message=message)
+    if response:
+        return decrypted_data
+    else:
+        logging.warning('Key will not be sent as notification backend did not respond properly.')
+        return
 
 
 if __name__ == '__main__':
@@ -36,4 +53,19 @@ if __name__ == '__main__':
             setattr(app, '_encrypted_from_env', True)
         else:
             raise ValueError('No file nor env var.')
+    with open(join(curdir, 'etc', 'notary.yaml'), 'rb') as configuration_data_file_handler:
+        configuration_data = safe_load(configuration_data_file_handler)
+        if 'notifications' in configuration_data:
+            if 'type' in configuration_data['notifications']:
+                notification_backend = configuration_data['notifications']['type']
+            elif not environ.get('NOTIFICATION_BACKEND'):
+                notification_backend = environ.get('NOTIFICATION_BACKEND')
+            else:
+                raise ValueError(f'No notification backend defined.')
+    if not notification_manager.select_backend(notification_backend):
+        raise ValueError(f'Notification backend "{notification_backend}" is not valid')
+
+    notification_manager.backend_module.configure(configuration=configuration_data['notifications'])
+    if not notification_manager.backend_module.validate_configuration():
+        exit(1)
     app.run(host='0.0.0.0')
